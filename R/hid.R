2a93d4d8bf1108e42a01f15b3f7ea7d648e3b392
Sean Carhart <carhartsd@gmail.com>
2025-07-18 14:43:51 -0700

    Added function documentation to file. Added input validation to read_hid.

--- R/hid.R
+++ R/hid.R
@@ -27,8 +27,16 @@ hid <- function() {
 
 }
 
+#' Reads an hid file
+#'
+#' @param filepath Character vector of length 1 containing path to the .fsa or .hid file.
+#' @param ... Additional arguments to pass to `readBin()`
+#'
+#' @returns Raw vector with contents of the file.
+#'
 read_hid <- function(filepath, ...) {
   stopifnot(file.exists(filepath))
+  stopifnot(length(filepath) == 1)
 
   hid_file <- file(filepath, open = "rb")
 
@@ -39,6 +47,12 @@ read_hid <- function(filepath, ...) {
   out
 }
 
+#' Extracts the ABIF header
+#'
+#' @param raw_data Raw vector with contents of .fsa or .hid file.
+#'
+#' @returns List with elements corresponding to the ABIF header information.
+#'
 extract_header <- function(raw_data) {
   list(
     file_format = rtc(raw_data[1:4]),
@@ -55,6 +69,14 @@ extract_header <- function(raw_data) {
   )
 }
 
+#' Extracts the ABIF directory.
+#'
+#' @param raw_data Raw vector containing the contents of a .fsa or .hid file.
+#' @param directory_offset The 0-based offset of the directory in the file. This is not the offset in the raw_data vector, which would larger by 1 due to R's 1-based indexing.
+#' @param num_elements Integer containing the number of directory entries.
+#'
+#' @returns List containing a list for each directory entry.
+#'
 extract_directory <- function(raw_data, directory_offset, num_elements) {
   stopifnot(is.raw(raw_data))
   stopifnot(is.integer(directory_offset))
@@ -70,6 +92,12 @@ extract_directory <- function(raw_data, directory_offset, num_elements) {
   })
 }
 
+#' Extracts the information for a single ABIF directory entry
+#'
+#' @param raw_data Raw vector of length 28 representing the directory entry.
+#'
+#' @returns List with elements corresponding to the ABIF directory entry information.
+#'
 extract_directory_entry <- function(raw_data) {
   stopifnot(is.raw(raw_data))
   stopifnot(length(raw_data) == 28)
